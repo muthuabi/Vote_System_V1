d4f9a1604f20268d25b80fb30f81b03a0005c1b3
muthuabi <muthuabi292@gmail.com>
2025-01-11 01:10:59 +0530

    Solved Individual post offcanvas with commenting

--- sub-admin/individual-post.php
+++ sub-admin/individual-post.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Individual Post</title>
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+<!--     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
     <script src='../assets/bootstrap/js/jquery.min.js'></script>
     <script src='../assets/bootstrap/js/bootstrap.bundle.min.js'></script>
     <link rel="stylesheet" href="../assets/css-js/styles.css">
     <script src='../assets/css-js/script.js'></script>
-    <script src="../assets/css-js/head_script.js"></script>
+    <script src="../assets/css-js/head_script.js"></script> -->
 </head>
 <?php if(isset($_GET['post_id']) && !empty(trim($_GET['post_id'])))
         {
